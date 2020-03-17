5f0dfff2d7bffb2c6211477e148080e1bf2d5490
Jose Maria CL <chema9703@gmail.com>
2020-03-16 22:32:57 -0600

    Verifying/counting mounted components

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -5,7 +5,7 @@ import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
 describe('<Header />', () => {
-  test('Render del componente Header', () => {
+  test('Render del componente del Header', () => {
     const header = shallow(
       <ProviderMock>
         <Header />
@@ -13,7 +13,8 @@ describe('<Header />', () => {
     );
     expect(header.length).toEqual(1);
   });
-  test('Render del Titulo', () => {
+
+  test('Render del título', () => {
     const header = mount(
       <ProviderMock>
         <Header />
