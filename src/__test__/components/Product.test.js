8c17c1166998acb862e62920cda9b5c05acfe8d5
Jose Maria CL <chema9703@gmail.com>
2020-03-16 22:33:25 -0600

    Simulate events

--- src/__test__/components/Product.test.js
+++ src/__test__/components/Product.test.js
@@ -4,7 +4,7 @@ import ProviderMock from '../../__mocks__/ProviderMock';
 import ProductMock from '../../__mocks__/ProductMock';
 import Product from '../../components/Product';
 
-describe('<Product/>', () => {
+describe('<Product />', () => {
   test('Render del componente Product', () => {
     const product = shallow(
       <ProviderMock>
@@ -13,14 +13,14 @@ describe('<Product/>', () => {
     );
     expect(product.length).toEqual(1);
   });
-  test('Comprobar el boton de comprar', () => {
+
+  test('Comprobar el boón de comprar', () => {
     const handleAddToCart = jest.fn();
     const wrapper = mount(
       <ProviderMock>
         <Product
           product={ProductMock}
-          handleAddToCart={handleAddToCart}
-        />
+          handleAddToCart={handleAddToCart} />
       </ProviderMock>,
     );
     wrapper.find('button').simulate('click');
